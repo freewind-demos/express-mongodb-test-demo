5ff0899c0dd64e48c1a0c7125e6652a0216e5929
Peng.Li <nowind_lee@qq.com>
2016-08-18 15:50:49 +0800

    use async to make test easier to read

--- spec/students-spec.js
+++ spec/students-spec.js
@@ -1,53 +1,44 @@
+'use strict';
 import request from 'supertest';
 import finish from './finish';
 import {Student} from '../app/db/schema';
 import db from '../app/db/db';
 import app from '../app/server';
+import async from 'async';
 
 describe('students', () => {
+
   beforeEach((done) => {
-    db.connect('test', (err) => {
-      if (err) return done.fail(err);
-      Student.find().remove(finish(done));
-    });
+    async.series([
+      (cb) => db.connect('test', cb),
+      (cb) => Student.find().remove(cb)
+    ], finish(done));
   });
+
   afterEach((done) => {
     db.close(finish(done));
   });
 
   it('init', (done) => {
-    request(app)
-      .post('/init')
-      .expect(200, function (err) {
-        if (err) return done.fail(err);
-
-        Student.find(function (err, users) {
-          if (err) return done.fail(err);
-
-          expect(users.length).toEqual(23);
-          done();
-        });
-      });
+    async.waterfall([
+      (cb) => request(app).post('/init').expect(200, cb),
+      (res, cb) => Student.find(cb),
+      (students, cb) => {
+        expect(students.length).toEqual(23);
+        cb();
+      }
+    ], finish(done));
   });
 
   it('delete user', (done) => {
-    new Student({"name": "Lily", "gender": "女"})
-      .save(function (err, savedUser) {
-        if (err) return done.fail(err);
-
-        request(app)
-          .delete('/students/' + savedUser._id)
-          .expect(204, function (err, res) {
-            if (err) return done.fail(err);
-
-            Student.find(function (err, students) {
-              if (err) return done.fail(err);
-
-              expect(students.length).toEqual(0);
-              done();
-            });
-          });
-      })
+    async.waterfall([
+      (cb) => new Student({"name": "Lily", "gender": "女"}).save((err, data) => cb(err, data)),
+      (lily, cb) => request(app).del('/students/' + lily._id).expect(204, cb),
+      (res, cb) => Student.find(cb),
+      (students, cb) => {
+        expect(students.length).toEqual(0);
+        cb();
+      }
+    ], finish(done));
   })
-
 });
